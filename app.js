cb3860d5ec519973071483b76abb20fb4babdbf8
Pavel Demin <pavlikdemin@yandex.ru>
2023-06-08 18:50:26 +0300

    add MongoDB

--- app.js
+++ app.js
@@ -4,14 +4,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
+
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.json());
 app.use(router);
-
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1/mestodb');
 
 app.listen(PORT, () => {
   console.log(`Слушаю порт ${PORT}`);
-}
-)
+});
--- controllers/users.js
+++ controllers/users.js
@@ -2,45 +2,38 @@
 /* eslint-disable no-console */
 /* eslint-disable linebreak-style */
 /* eslint-disable no-multiple-empty-lines */
-const express = require('express');
-
-const users = [];
-let id = 0;
-
+const User = require('../models/user');
+// const users = [];
+// let id = 0;
 const getUsers = (req, res) => {
-  console.log('это GET запрос на /юзерс');
-  res.status(200).send(users);
+  // console.log('это GET запрос на /юзерс');
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => res.status(500).send({ "message": 'Internal Server Error', err: err.message, stack: err.stack }));
 };
-
 
 const getUserById = (req, res) => {
-  console.log('это GET запрос на /юзерс');
-  console.log(req.params.userId);
-  const { userId } = req.params;
-  const user = users.find((item) => item.id === Number(userId));
+  User.findById(req.params.userId)
+    .orFail(() => new Error('not found'))
+    .then((user) => res.status(200).send(user))
+    .catch((err) => res.status(500).send({ "message": 'Internal Server Error', err: err.message, stack: err.stack }));
+  // const user = users.find((item) => item.id === Number(userId));
 
-  if (user) {
-    return res.status(200).send(user);
-  }
-  return res.status(404).send({"message": "User not found"});
+  // if (user) {
+  //   return res.status(200).send(user);
+  // }
+  // return res.status(404).send({ 'message': 'User not found' });
 };
 
-
-
 const createUser = (req, res) => {
-  console.log('это POST запрос на /юзерс');
-  console.log("req.body", req.body);
-  id+=1;
-  const newUser = {
-    id,
-    ...req.body,
-  }
-  users.push(newUser);
-  res.status(201).send(newUser);
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => res.status(500).send('Internal error'));
 };
 
 module.exports = {
   createUser,
   getUserById,
-  getUsers
+  getUsers,
 };
