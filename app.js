063c3b57d08b9cb3cd14de7cc86adeb3e4992641
Pavel Demin <pavlikdemin@yandex.ru>
2023-06-29 01:03:30 +0300

    ПР15. Добавил логирование запросов и ошибок

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const router = require('./routes');
 // const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -14,8 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1/mestodb');
 
+app.use(requestLogger);
 app.use(cookieParser());
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 app.listen(PORT, () => {
